4a6431c9baf99380369ab62805724e2ab10bc56f
Eunnee <rlawldms4828@naver.com>
2022-09-14 17:56:52 +0900

    message

---
+++ configserver/src/test/java/kr/co/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
